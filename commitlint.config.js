de9dced04132d147360a8a9165624eea41c18d14
Gabriel Aquino <gabriel.aquino@outlook.com>
2026-02-25 17:22:45 -0300

    chore: update dependencies and tooling config

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,23 +2,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-      ],
-    ],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
     'header-max-length': [2, 'always', 100],
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
   },
