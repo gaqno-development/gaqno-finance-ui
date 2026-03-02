b19407855565688cd2d1931297dc2f7bb1b65f4b
Gabriel Aquino <gabriel.aquino@outlook.com>
2026-03-02 08:54:23 -0300

    fix(i18n): add initI18n and I18nProvider to App.tsx for MFE mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { AuthProvider, QueryProvider, TenantProvider } from '@gaqno-development/frontcore'
+import { initI18n, I18nProvider } from '@gaqno-development/frontcore/i18n'
 import { FinancePage } from './pages/FinancePage'
+
+initI18n()
 
 export default function App() {
   return (
-    <QueryProvider>
-      <AuthProvider>
-        <TenantProvider>
-          <FinancePage />
-        </TenantProvider>
-      </AuthProvider>
-    </QueryProvider>
+    <I18nProvider>
+      <QueryProvider>
+        <AuthProvider>
+          <TenantProvider>
+            <FinancePage />
+          </TenantProvider>
+        </AuthProvider>
+      </QueryProvider>
+    </I18nProvider>
   )
 }
