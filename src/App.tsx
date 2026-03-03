00cf51c1a937f561d2bd1b5238a639b114fb4820
Gabriel Aquino <gabriel.aquino@outlook.com>
2026-03-03 11:22:19 -0300

    chore(gaqno-finance-ui): update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import React from 'react'
-import { AuthProvider, QueryProvider, TenantProvider } from '@gaqno-development/frontcore'
-import { initI18n, I18nProvider } from '@gaqno-development/frontcore/i18n'
-import { FinancePage } from './pages/FinancePage'
+import React from "react";
+import { initI18n, I18nProvider } from "@gaqno-development/frontcore/i18n";
+import { FinancePage } from "./pages/FinancePage";
 
-initI18n()
+initI18n();
 
 export default function App() {
   return (
     <I18nProvider>
-      <QueryProvider>
-        <AuthProvider>
-          <TenantProvider>
-            <FinancePage />
-          </TenantProvider>
-        </AuthProvider>
-      </QueryProvider>
+      <FinancePage />
     </I18nProvider>
-  )
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import {
+  AuthProvider,
+  QueryProvider,
+  TenantProvider,
+} from "@gaqno-development/frontcore";
+import App from "./App";
 import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <QueryProvider>
+        <AuthProvider>
+          <TenantProvider>
+            <App />
+          </TenantProvider>
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
